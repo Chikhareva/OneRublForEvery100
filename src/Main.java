64dec8837f9c765a7f1a59f17484848ef890d54c
Vera Chikhareva <vera.chepovaya@mail.ru>
2020-12-24 13:32:22 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int refill=25;
-        int total;
-        int bonus;
-        if ( refill>1000 ) ;
-            bonus = refill / 100;
-            total = balance + refill;
-        if ( refill<=1000 )
-            bonus = 0;
-            total = balance + refill;
+        int payment = 14;
+        long total;
+        long bonus;
+        if (payment > 1000) {
+            bonus = payment / 100;
+            total = balance + payment;
             System.out.println(bonus);
             System.out.println(total);
         }
+        if (payment <= 1000) {
+            bonus = 0;
+            total = balance + payment;
+            System.out.println(bonus);
+            System.out.println(total);
         }
+    }
+}
+
 
 
 
